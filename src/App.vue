6e5f75bccd158845674cc5836afd52931f807a4b
= <=>
2024-04-18 14:14:03 +0400

    initial commit,vuetify is installed

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import TodoList from './components/TodoList/TodoList.vue'
+
+export default {
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<template>
+  <main>
+    <TodoList />
+  </main>
+</template>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './assets/main.css'
+import 'vuetify/styles'
+
+import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import App from './App.vue'
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
+createApp(App).use(vuetify).mount('#app')
