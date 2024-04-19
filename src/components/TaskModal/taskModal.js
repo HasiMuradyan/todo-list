b207447d784bbb817ee6938e6985eeee8a399b92
= <=>
2024-04-20 00:14:04 +0400

    Task Modal is added

---
+++ src/components/TaskModal/taskModal.js
@@ -0,0 +1,41 @@
+import Datepicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+
+export default {
+  components: {
+    Datepicker
+  },
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: true
+    },
+    isEmpty: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      title: '',
+      description: '',
+      dueDate: ''
+    }
+  },
+  methods: {
+    onClose() {
+      this.$emit('close')
+    },
+    onSave() {
+      const newTask = {
+        title: this.title,
+        description: this.description,
+        date: this.dueDate
+      }
+      this.$emit('taskSave', newTask)
+    },
+    onTitleInput(event) {
+      this.title = event.target.value
+    }
+  }
+}
--- src/components/TodoList/todoList.js
+++ src/components/TodoList/todoList.js
@@ -1,1 +1,32 @@
-export default {}
+import TaskModal from '../TaskModal/TaskModal.vue'
+export default {
+  components: {
+    TaskModal
+  },
+  data() {
+    return {
+      isTaskModalOpen: false,
+      isTitleEmpty: false,
+      tasks: []
+    }
+  },
+  methods: {
+    toggleTaskModal() {
+      this.isTaskModalOpen = !this.isTaskModalOpen
+      this.isTitleEmpty = false
+    },
+
+    onTaskSave(task) {
+      if (task.title.trim() !== '') {
+        this.tasks.push(task)
+        this.toggleTaskModal()
+        this.isTaskModalOpen = false
+      } else {
+        this.isTitleEmpty = true
+        this.isTaskModalOpen = true
+      }
+
+      console.log(this.tasks)
+    }
+  }
+}
