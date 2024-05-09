09418f9d56412983ba91bb47aa66de55eb509fda
= <=>
2024-05-10 02:16:48 +0400

    header,footer,loader, not found,single task components are added

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,11 +1,1 @@
-export default {
-  data() {
-    return {
-      navBars: [
-        { title: 'Home', path: '/' },
-        { title: 'About Us', path: '/about' },
-        { title: 'Contact Us', path: '/contact-us' }
-      ]
-    }
-  }
-}
+export default {}
